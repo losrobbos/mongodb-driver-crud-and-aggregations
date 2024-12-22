764637b812373c044a6cfcb4440e70afeab7b987
losrobbos <robert.ristock@googlemail.com>
2024-12-22 13:08:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Test cases performing MongoDB actions
+
+All tests use the Node MongoDB driver (package mongodb).
+
+The tests are executed using the package "vitest".
+
+## How to run
+
+`npm install`
+`npm test`
+
---
+++ mongo-crud-aggregate.test.js
@@ -0,0 +1,108 @@
+import { MongoClient, Db, Collection } from "mongodb";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+
+const MONGO_URI = "mongodb://localhost"
+
+describe("Test MongoDB Driver CRUD", () => {
+
+    /** @type MongoClient */
+    let client
+    /** @type Db */
+    let db
+    /** @type Collection */
+    let coll
+
+    // connect to Mongo Client & create db + collection
+    beforeAll(async () => {
+        client = await MongoClient.connect(MONGO_URI)
+        db = client.db("palimpalim")
+        coll = await db.createCollection("todos")
+        await coll.insertMany([
+            { user: "rob", title: "Do some TDD setup", status: "DONE"},
+            { user: "rob", title: "Do some Teardown", status: "DONE"},
+            { user: "rob", title: "Add some sample data", status: "IN PROGRESS"},
+            { user: "rob", title: "Do Mongoose CRUD", status: "IN PROGRESS"},
+            { user: "rob", title: "Show some find Operators", status: "OPEN"},
+            { user: "rob", title: "Do some Aggregation", status: "OPEN"},
+            { user: "rob", title: "Do some more Aggregation", status: "OPEN"},
+            { user: "rob", title: "Do even more Aggregation", status: "OPEN"},
+        ])
+
+    })
+
+    // teardown
+    afterAll(async () => {
+        await db.dropDatabase()
+        client.close()
+    })
+
+    // TESTS...
+    test("create document", async () => {
+        const doc = await coll.insertOne({ title: "Do some TDD", status: "IN PROGRESS" })
+        expect(doc.acknowledged).toBe(true)
+    })
+
+    test("find document", async () => {
+        const doc = await coll.findOne()
+        expect(doc).not.toBeNull()
+        expect(doc._id).toBeDefined()
+    })
+
+    test("find all todos", async () => {
+        const cursor = coll.find()
+
+        // expect to find a doc
+        let hasnext = await cursor.hasNext()
+        expect(hasnext).toBe(true)
+
+        // get next doc
+        // const doc = await cursor.next()
+        // expect(doc._id).toBeTruthy()
+        // expect(doc.title).toBeTruthy()
+
+        const docs = await cursor.toArray()
+        expect(docs.length).toBe(9)
+        expect(docs[0]._id).toBeTruthy()
+
+        // testing promise resolve
+        // expect that cursor is exhausted
+        // expect(cursor.hasNext()).resolves.toBe(false) // deprecated
+
+        hasnext = await cursor.hasNext()
+        expect(hasnext).toBe(false)
+    })
+
+    test("find by criteria", async () => {
+        const docs = await coll.find({
+            status: "DONE"
+        }).toArray()
+        expect(docs.length).toBe(2)
+        expect(docs.at(-1).title).toBe("Do some Teardown")
+    })
+
+    test("find limit results / pages", async () => {
+        // page 1 records (first 3)
+        let docs = await coll.find({ }).limit(3).toArray()
+        expect(docs.length).toBe(3)
+        expect(docs[0].title).toBe("Do some TDD setup")
+        
+        // page 2 records (next 3)
+        docs = await coll.find({ }).skip(3).limit(3).toArray()
+        expect(docs.length).toBe(3)
+        expect(docs[0].title).toBe("Do Mongoose CRUD")        
+    })
+
+    test("aggregate group", async () => {
+        const cursor = coll.aggregate([
+            {
+                $group: {
+                    _id: "$status",
+                    docs: { $count: {} },
+                }
+            }
+        ])
+        const result = await cursor.toArray()
+        console.log(result)
+    })
+
+})
